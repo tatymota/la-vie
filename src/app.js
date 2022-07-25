5c0f486ffcdca1c20e9b3685c3dd4f45ab04364f
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-07-25 17:16:25 -0300

    feat: create database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
 
+const db = require("./database");
+
 const app = express();
+
+db.hasConection();
 
 // essa função precisa está antes das rotas, para conseguir rodar
 app.use(express.json());
