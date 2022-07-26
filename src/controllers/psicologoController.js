93b82baaf6a42cd99ba8b07212667cb7daa95807
LeoMPG <105465283+LeoMPG@users.noreply.github.com>
2022-07-26 16:14:17 -0300

    post e get ok

---
+++ src/controllers/psicologoController.js
@@ -0,0 +1,31 @@
+const {Psicologos} = require("../models");
+const psicologoController = {
+    listarPsicologos: async (req, res) => {
+        const listarPsicologos = await Psicologos.findAll();
+
+        res.json(listarPsicologos)
+    },
+
+
+async cadastrarPsicologos(req, res) {
+    const { nome, email, senha, apresentacao } = req.body
+    
+    const novoPsicologo = await Psicologos.create({
+        nome, 
+        email, 
+        senha,
+        apresentacao
+    })
+
+    res.json(novoPsicologo);
+},
+ async deletarPsicologos(req, res){
+    const { id } = req.params;
+    await Psicologos.destroy({
+        where:{
+            id
+        },
+    });
+    res.json ("Psicologo deletado com sucesso!")
+ }}
+module.exports = psicologoController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const pacienteController = require("../controllers/pacienteController");
+const psicologoController = require("../controllers/psicologoController");
 
 const routes = express.Router();
 
 routes.get("/paciente/lista", pacienteController.listarPaciente);
 routes.post("/paciente/criar", pacienteController.cadastrarPaciente);
-routes.delete("/paciente/:id/deletar", pacienteController.deletarProduto);
+routes.get("/psicologos", psicologoController.listarPsicologos);
+routes.post("/psicologos", psicologoController.cadastrarPsicologos);
+routes.delete("/psicologos", psicologoController.deletarPsicologos);
 
 module.exports = routes;
