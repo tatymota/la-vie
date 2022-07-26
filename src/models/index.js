d813ce63e834b121df2c3b4e4c8d433e1e843652
eduardohpb <100959169+eduardohpb@users.noreply.github.com>
2022-07-25 23:27:52 -0300

    feat(Criando Relacionamento)
    Criando o Relacionamento entre as tabelas dos modes.

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,14 @@ const Atendimento = require("./Atendimento");
 const Pacientes = require("./Pacientes");
 const Psicologos = require("./Psicologos");
 
+Psicologos.belongsTo(Atendimento,{
+    foreignKey: "psicologos_id"
+});
+Pacientes.belongsTo(Atendimento, {
+    foreingKey: "pacientes_id",
+    });
+
+
 module.exports = {
     Atendimento, 
     Pacientes,
